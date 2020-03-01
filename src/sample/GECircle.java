f1aa55c2669130181dd0133a0fc284672166608d
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2020-03-01 22:35:52 +0300

    Updated GECircle and GERegularPolygon

--- src/sample/GECircle.java
+++ src/sample/GECircle.java
@@ -4,22 +4,8 @@ import javafx.scene.shape.Circle;
 
 public class GECircle extends GEGeometry {
 
-    private static double[] getPlatonicPoints(double radius, int divisionCount, double alphaOffset){
-        double[] buffVertices = new double[divisionCount*2];
-        double alpha = alphaOffset;
-        for(int i=0; i<buffVertices.length; i+=2){
-            buffVertices[i] = Math.cos(alpha)*radius;
-            buffVertices[i+1] = Math.sin(alpha)*radius;
-            alpha = alpha + 2*Math.PI/divisionCount;
-        }
-        return buffVertices;
-    }
-
     public GECircle(double radius){
         super(new Circle(radius));
     }
 
-    public GECircle(double radius, int divisionCount, double alphaOffset){
-        super(getPlatonicPoints(radius, divisionCount, alphaOffset));
-    }
 }
--- src/sample/GERegularPolygon.java
+++ src/sample/GERegularPolygon.java
@@ -1,11 +1,21 @@
 package sample;
 
-import javafx.scene.shape.Circle;
 
-public class GERegularPolygon extends GECircle {
+public class GERegularPolygon extends GEGeometry {
+
+    private static double[] getPlatonicPoints(double radius, int divisionCount){
+        double[] buffVertices = new double[divisionCount*2];
+        double alpha = 0;
+        for(int i=0; i<buffVertices.length; i+=2){
+            buffVertices[i] = Math.cos(alpha)*radius;
+            buffVertices[i+1] = Math.sin(alpha)*radius;
+            alpha = alpha + 2*Math.PI/divisionCount;
+        }
+        return buffVertices;
+    }
 
     public GERegularPolygon(double radius, int divisionCount){
-        super(radius,divisionCount,0);
+        super(getPlatonicPoints(radius,divisionCount));
     }
 
 }
