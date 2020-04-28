7eb659618099b32f754743eb9e778e019d4d34bd
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2020-04-28 16:57:00 +0300

    Update GELine.java

--- src/ge/geometry/GELine.java
+++ src/ge/geometry/GELine.java
@@ -1,11 +1,28 @@
 package ge.geometry;
 
+import ge.utils.GEBoundingBox;
 import ge.utils.GERegularBoundingBox;
 
-public class GELine extends GERegularPolygon {
+public class GELine extends GEGeometry {
+
+    private static double[] getLinePoints(GEBoundingBox boundingBox){
+        double[] buffVertices = new double[4]; // [x, y] 4 times
+
+        buffVertices[0] = boundingBox.getPoint1().x;
+        buffVertices[1] = boundingBox.getPoint1().y;
+
+        buffVertices[2] = boundingBox.getPoint2().x;
+        buffVertices[3] = boundingBox.getPoint2().y;
+
+        return buffVertices;
+    }
+
+    public GELine(GEBoundingBox boundingBox){
+        super(getLinePoints(boundingBox));
+    }
 
     public GELine(GERegularBoundingBox boundingBox){
-        super(boundingBox.size/2, 2, Math.PI/4);
+        super(getLinePoints(boundingBox));
     }
 
 }
